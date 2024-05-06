4ddb4a6b4ae6bc4ca0d0f09d131ae1a1d9e4cd54
Deepika Goel <80449168+deepikagoel1@users.noreply.github.com>
2024-05-06 16:25:01 +0530

    Add files via upload
    Uploaded files via GUI

--- lms_frontend/src/index.js
+++ lms_frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+// import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -13,13 +14,16 @@ import {BrowserRouter} from 'react-router-dom';
 //   </React.StrictMode>
 // );
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'));
+
+
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
+  // document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
