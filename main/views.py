01ab2c38b97cd3de4c6b597867d7b5e8b0ed6df0
Deepika <goeld9911@gmail.com>
2024-05-07 13:12:54 +0530

    implemented teacher-login and added to a path urls.py and with that modified views.py if teacher is login into the portal than Dashboard and logout option should be made visible

--- main/views.py
+++ main/views.py
@@ -7,6 +7,10 @@ from . import models
 from rest_framework.response import Response
 from rest_framework import generics
 from rest_framework import permissions
+from django.http import JsonResponse, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.datastructures import MultiValueDictKeyError
+
 
 # class TeacherList(APIView):
 #     def get(self, request):  #Using post method instead of get method from API
@@ -23,3 +27,14 @@ class TeacherDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Teacher.objects.all()
     serializer_class = TeacherSerializer
     # permission_classes = [permissions.IsAuthenticated]
+
+@csrf_exempt
+def teacher_login(request):
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+    teacherData = models.Teacher.objects.filter(email = email, password = password).first()
+    if teacherData:
+        return JsonResponse({'bool': True})
+    else:
+        return JsonResponse({'bool': False})
+    return render(request)
